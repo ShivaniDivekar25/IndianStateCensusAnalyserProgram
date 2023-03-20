62da374898185eae1a6c2b5c2000e2ba22c1375f
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-03-20 19:17:48 +0530

    Modified to Load Indian State Census Information From CSV File

---
+++ IndianStateCensusAnalyser/CsvStateCensus.cs
@@ -0,0 +1,29 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser
+{
+    public class CsvStateCensus
+    {
+        public void ReadStateCensusData(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<StateCensusModel>().ToList();
+                    foreach (var record in records)
+                    {
+                        Console.WriteLine($"{record.State} {record.Population} {record.AreaInSqKm} {record.DensityPerSqKm}");
+                    }
+                }
+            }
+        }
+    }
+}
--- IndianStateCensusAnalyser/Program.cs
+++ IndianStateCensusAnalyser/Program.cs
@@ -2,9 +2,12 @@
 {
     internal class Program
     {
+        public static string filePath = @"C:\Users\hp\source\repos\IndianStateCensusAnalyser\IndianStateCensusAnalyser\Files\StateCensusData.csv";
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Indian State Census Analyser Program.");
+            CsvStateCensus csvStateCensus = new CsvStateCensus();
+            csvStateCensus.ReadStateCensusData(filePath);
         }
     }
 }
