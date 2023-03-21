eb403ced4f8b93de1dec3fa0809293bd65dbaeb7
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2023-03-21 08:25:41 +0530

    Modified to Load Indian State Code Information From CSV File

---
+++ IndianStateCensusAnalyser/CsvStateCode.cs
@@ -0,0 +1,29 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace IndianStateCensusAnalyser
+{
+    public class CsvStateCode
+    {
+        public void ReadStateCodeData(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<StateCodeModel>().ToList();
+                    foreach (var record in records)
+                    {
+                        Console.WriteLine($"{record.SrNo} {record.Name} {record.TIN} {record.StateCode}");
+                    }
+                }
+            }
+        }
+    }
+}
--- IndianStateCensusAnalyser/Program.cs
+++ IndianStateCensusAnalyser/Program.cs
@@ -3,11 +3,14 @@
     internal class Program
     {
         public static string filePath = @"C:\Users\hp\source\repos\IndianStateCensusAnalyser\IndianStateCensusAnalyser\Files\StateCensusData.csv";
+        public static string filePath1 = @"C:\Users\hp\source\repos\IndianStateCensusAnalyser\IndianStateCensusAnalyser\Files\StateCode.csv";
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Indian State Census Analyser Program.");
             CsvStateCensus csvStateCensus = new CsvStateCensus();
             csvStateCensus.ReadStateCensusData(filePath);
+            CsvStateCode csvStateCode = new CsvStateCode();
+            csvStateCode.ReadStateCodeData(filePath1);
         }
     }
 }
